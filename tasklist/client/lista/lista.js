b2fa0c93eeddc5692c57dafa19212ae5f5fdd720
Geison Santos <geison.sn@gmail.com>
2016-02-18 20:42:23 -0300

    Aula 02 do curso de Meteor do Alura: consultando dados com MongoDB

--- tasklist/client/lista/lista.js
+++ tasklist/client/lista/lista.js
@@ -1,9 +1,5 @@
 Template.lista.helpers({
 	tarefas : function() {
-		return [
-			{nome : "Ir ao mercado"},
-			{nome : "Comprar sorvete"},
-			{nome : "Estudar"}
-		];
+		return Tarefas.find({});
 	}
 });
